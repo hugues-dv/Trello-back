15e5feb2b76f486d98643077103b784189cf4c47
hugues-dv <h2vregille@gmail.com>
2024-01-05 09:32:45 +0100

    update sql crea db

--- Trello.sql
+++ Trello.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE IF NOT EXISTS Trello_db; 
 
 -- SQLite
-CREATE TABLE IF NOT EXISTS Projects (
+CREATE TABLE IF NOT EXISTS Project (
     id INT PRIMARY KEY,
     name varchar (50),
     description varchar (50),
     createdAt DATETIME
 ); 
 
-CREATE TABLE IF NOT EXISTS Lists (
+CREATE TABLE IF NOT EXISTS List (
     id INT PRIMARY KEY,
     name varchar (50),
 );
@@ -17,26 +17,26 @@ CREATE TABLE IF NOT EXISTS ProjectListAssociation (
     projectId INT,
     listId INT,
     PRIMARY KEY (projectId, listId),
-    FOREIGN KEY (projectId) REFERENCES Projects(id) ON DELETE CASCADE,
-    FOREIGN KEY (listId) REFERENCES Lists(id) ON DELETE CASCADE
+    FOREIGN KEY (projectId) REFERENCES Project(id) ON DELETE CASCADE,
+    FOREIGN KEY (listId) REFERENCES List(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS Cards (
+CREATE TABLE IF NOT EXISTS Card (
     id INT PRIMARY KEY ,
     title varchar (50),
     description varchar (50),
     createdAt DATETIME,
     idList INT,
-    FOREIGN KEY (IdList) REFERENCES lists(Id) ON DELETE CASCADE
+    FOREIGN KEY (IdList) REFERENCES List(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS Comments (
+CREATE TABLE IF NOT EXISTS Comment (
     id INT PRIMARY KEY,
     content varchar (50),
     createdAt DATETIME,
     idCard INT,
     user varchar (50),
-    FOREIGN KEY (IdCard) REFERENCES Cards(Id) ON DELETE CASCADE
+    FOREIGN KEY (IdCard) REFERENCES Card(Id) ON DELETE CASCADE
 );
 
 

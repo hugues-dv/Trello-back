42b2a51c24940c3b56d0e5b937a55d4aba1e652b
hugues-dv <h2vregille@gmail.com>
2024-01-22 10:54:15 +0100

    retour sur sqlite

--- Models/TrelloContext.cs
+++ Models/TrelloContext.cs
@@ -27,12 +27,12 @@ public partial class TrelloContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        =>
-        optionsBuilder.UseSqlServer(
-            "Server=tcp:trello.database.windows.net,1433;Initial Catalog=Trello;Persist Security Info=False;User ID=Groupe2;Password=M1n1Tr3770;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-        );
+    // =>
+    // optionsBuilder.UseSqlServer(
+    //     "Server=tcp:trello.database.windows.net,1433;Initial Catalog=Trello;Persist Security Info=False;User ID=Groupe2;Password=M1n1Tr3770;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    // );
 
-    // => optionsBuilder.UseSqlite("Data Source=Trello.db");
+    => optionsBuilder.UseSqlite("Data Source=Trello.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

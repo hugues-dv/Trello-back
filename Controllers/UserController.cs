e85460fc84e401066174abd1df341c56834a12d6
Alexandre BAUCHET <alex.bauchet95160@gmail.com>
2024-01-25 10:41:43 +0100

    auth jwt ok routes non secures

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,53 +1,103 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BCrypt.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Trello_back.Models;
 
-[Route("api/User")]
-[ApiController]
-public class UserController : ControllerBase
+namespace Trello_back.Controllers
 {
-    private readonly TrelloContext _context;
-
-    public UserController(TrelloContext context)
+    [Route("/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
-        _context = context;
-    }
+        private readonly TrelloContext _context;
+        private readonly IConfiguration _configuration;
 
-    private bool ValidateUser(string username, string password)
-    {
-        // Implémentez la logique de validation ici
-        return true;
-    }
-
-    [HttpPost("register")]
-    public IActionResult Register([FromBody] User user)
-    {
-        // Ajoutez l'utilisateur à votre base de données
-        // Gérez les erreurs et les validations
-        return Ok();
-    }
-
-    [HttpPost("login")]
-    public IActionResult Login([FromBody] User user)
-    {
-        if (ValidateUser(user.Username, user.Password))
+        public UserController(TrelloContext context, IConfiguration configuration)
         {
-            var tokenString = GenerateJWTToken(user.Username);
-            return Ok(new { Token = tokenString });
+            _context = context;
+            _configuration = configuration;
         }
-        return Unauthorized();
-    }
 
-    private string GenerateJWTToken(string username)
-    {
-        // Générez ici votre JWT
-        return "token";
-    }
+        private bool ValidateUser(string username, string password)
+        {
+            // Implémentez la logique de validation ici
+            return true;
+        }
 
-    private bool UserExists(string username)
-    {
-        return _context.Users.Any(e => e.Username == username);
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] User user)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(user));
+            if (ModelState.IsValid && !string.IsNullOrEmpty(user.Password))
+            {
+                // Vérifie si l'utilisateur existe déjà
+                var existingUser = _context.Users.FirstOrDefault(u => u.Username == user.Username);
+                if (existingUser != null)
+                {
+                    return BadRequest("Ce nom d'utilisateur est déjà utilisé.");
+                }
+
+                // Hachez le mot de passe avant de le stocker
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+                // Créez un nouvel utilisateur avec le mot de passe haché
+                User newUser = new User { Username = user.Username, Password = hashedPassword };
+
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+
+                return Ok("Inscription réussie.");
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User user)
+        {
+            var existingUser = _context.Users.FirstOrDefault(u => u.Username == user.Username);
+
+            if (
+                existingUser != null
+                && BCrypt.Net.BCrypt.Verify(user.Password, existingUser.Password)
+            )
+            {
+                var tokenString = GenerateJWTToken(existingUser.Username);
+                return Ok(new { Token = tokenString });
+            }
+            return Unauthorized("Nom d'utilisateur ou mot de passe incorrect.");
+        }
+
+        private string GenerateJWTToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])
+            );
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
